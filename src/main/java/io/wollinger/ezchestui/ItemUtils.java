394256640a6892d5172678e4ae48a36aaf3c298c
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-05-22 13:34:28 +0200

    Update ItemUtils.java

---
+++ src/main/java/io/wollinger/ezchestui/ItemUtils.java
@@ -0,0 +1,30 @@
+package io.wollinger.ezchestui;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.util.List;
+
+public class ItemUtils {
+    public static ItemStack getHead(String username) {
+        ItemStack item = new ItemStack(Material.PLAYER_HEAD, 1);
+        SkullMeta skull = (SkullMeta) item.getItemMeta();
+        skull.setOwner(username);
+        item.setItemMeta(skull);
+        return item;
+    }
+
+    public static ItemStack setInfo(ItemStack item, String name, List<String> desc) {
+        ItemMeta meta = item.getItemMeta();
+
+        if(name != null && !name.isEmpty())
+            meta.setDisplayName(ChatColor.WHITE + name);
+        if(desc != null && !desc.isEmpty())
+            meta.setLore(desc);
+        item.setItemMeta(meta);
+        return item;
+    }
+}
