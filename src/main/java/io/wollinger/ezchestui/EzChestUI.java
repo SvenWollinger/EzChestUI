01f98e10d201ce6ced3db77578268e5bcad242b0
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-05-22 12:18:37 +0200

    Initial commit

---
+++ src/main/java/io/wollinger/ezchestui/EzChestUI.java
@@ -0,0 +1,12 @@
+package io.wollinger.ezchestui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EzChestUI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
