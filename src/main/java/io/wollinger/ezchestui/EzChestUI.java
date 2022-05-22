f6ac333c24e49b90a2c3019ff4ae934a0ca0a899
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-05-22 13:12:32 +0200

    Update EzChestUI.java

--- src/main/java/io/wollinger/ezchestui/EzChestUI.java
+++ src/main/java/io/wollinger/ezchestui/EzChestUI.java
@@ -2,11 +2,22 @@ package io.wollinger.ezchestui;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class EzChestUI extends JavaPlugin {
+    private static EzChestUI instance;
+    private static Logger logger;
 
     @Override
     public void onEnable() {
+        instance = this;
+        UIManager.init(this);
+        instance.getCommand("demoui").setExecutor(new CMDDemoUI());
+    }
 
+    public static void log(String message, Level level) {
+        instance.getLogger().log(level, message);
     }
 
 }
