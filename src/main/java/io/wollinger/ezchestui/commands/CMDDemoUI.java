4d3db7bbe49db754e68da4c00591649972e03dba
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-05-23 13:06:25 +0200

    Adapting demo command

--- src/main/java/io/wollinger/ezchestui/commands/CMDDemoUI.java
+++ src/main/java/io/wollinger/ezchestui/commands/CMDDemoUI.java
@@ -2,7 +2,6 @@ package io.wollinger.ezchestui.commands;
 
 import io.wollinger.ezchestui.EzChestUI;
 import io.wollinger.ezchestui.uiparts.EzUI;
-import io.wollinger.ezchestui.uiparts.EzUIItem;
 import io.wollinger.ezchestui.utils.ItemUtils;
 import io.wollinger.ezchestui.UIManager;
 import net.md_5.bungee.api.ChatColor;
@@ -11,6 +10,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.logging.Level;
 
@@ -28,19 +28,15 @@ public class CMDDemoUI implements CommandExecutor {
 
         EzUI inv = UIManager.createInventory("Demo", 6);
 
-        EzUIItem headItem = new EzUIItem(ItemUtils.getHead("SvenWollinger"));
-        headItem.setName("Cool head!");
-        headItem.setLore("Line 1!\n" + ChatColor.WHITE + "Line 2!\nLine 3!");
-        headItem.setClickFunction(() -> sender.sendMessage("Cool!"));
+        ItemStack headItem = ItemUtils.getHead("SvenWollinger");
+        ItemUtils.setName(headItem, "Cool head!");
+        ItemUtils.setLore(headItem, "Line 1!\n" + ChatColor.WHITE + "Line 2!\nLine 3!");
+        inv.setItem(0, headItem, true, () -> sender.sendMessage("Cool !"));
 
-        EzUIItem funkyDirt = new EzUIItem(ItemUtils.getStack(Material.DIRT));
-        funkyDirt.setName("FUNKY dirt!");
-        funkyDirt.setLore("Note: Do not consume.");
-        funkyDirt.setClickFunction(() -> sender.sendMessage("Munch munch..."));
-        funkyDirt.setCloseOnClick(false);
-
-        inv.setItem(0, headItem);
-        inv.setItem(10, funkyDirt);
+        ItemStack funkyDirt = ItemUtils.getStack(Material.DIRT);
+        ItemUtils.setName(funkyDirt, "FUNKY dirt!");
+        ItemUtils.setLore(funkyDirt, "Note: Do not consume.");
+        inv.setItem(10, funkyDirt, false, () -> sender.sendMessage("Munch munch..."));
 
         UIManager.openInventory((Player) sender, inv);
 
